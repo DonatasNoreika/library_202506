06bdd9db512f3554741e66400477db7fd4c448db
DonatasNoreika <donoras@gmail.com>
2025-06-17 10:21:11 +0300

    3: +Index puslapyje atvaizduojama svetainės statistika

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, BookInstance, Author
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+    num_authors = Author.objects.all().count()
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+    }
+    return render(request, template_name="index.html", context=context)
