601a78d961c65e574738487649776aac2adb9463
DonatasNoreika <donoras@gmail.com>
2025-06-23 10:01:27 +0300

    6: +Slaptažodžių pakeitimo html šablonai pokeisti savais

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,6 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-pmigki%%bq^zj1+vn05_g(n@9zlk+$6gk^_)wzzy0k4au0vtpw'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+# el. pašto adresas iš kurio siųsite
+EMAIL_HOST_PASSWORD = 'VerySecret'
+# slaptažodis
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
