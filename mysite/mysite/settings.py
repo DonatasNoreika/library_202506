fd94692f55abe8a23b4566392f37c4cc3b6cfd4c
DonatasNoreika <donoras@gmail.com>
2025-06-23 09:45:14 +0300

    6: +Įdėta login/logout logika

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
 
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
