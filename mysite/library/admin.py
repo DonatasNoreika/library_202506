d36f782872f2a5b937a6536163d3c8eb615c27ca
DonatasNoreika <donoras@gmail.com>
2025-06-26 10:43:05 +0300

    8: +Prie knygos galima palikti komentarus (kol kas tik per admin)

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Book, Genre, BookInstance
+from .models import Author, Book, Genre, BookInstance, BookReview
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'display_books']
@@ -28,8 +28,12 @@ class BookInstanceAdmin(admin.ModelAdmin):
         ('Availability', {'fields': ('status', 'reader', 'due_back')}),
     )
 
+class BookReviewAdmin(admin.ModelAdmin):
+    list_display = ['book', 'date_created', 'reviewer', 'content']
+
 # Register your models here.
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
 admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -91,3 +91,15 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.book} ({self.uuid})"
+
+
+class BookReview(models.Model):
+    book = models.ForeignKey(to="Book", verbose_name="Knyga", on_delete=models.SET_NULL, null=True, blank=True, related_name="reviews")
+    reviewer = models.ForeignKey(to=User, verbose_name="Komentatorius", on_delete=models.SET_NULL, null=True, blank=True)
+    date_created = models.DateTimeField(verbose_name="Data", auto_now_add=True)
+    content = models.TextField(verbose_name="Atsiliepimas", max_length=2000)
+
+    class Meta:
+        verbose_name = "Atsiliepimas"
+        verbose_name_plural = 'Atsiliepimai'
+        ordering = ['-date_created']
